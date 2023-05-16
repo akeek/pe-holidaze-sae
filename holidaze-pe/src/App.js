916bb446d97a1ffcb930273f6ec11c6e9cb5af61
Åke Ek <svein.ek@stud.noroff.no>
2023-05-16 14:42:30 +0200

    remove unwanted stuff until local is ready

--- holidaze-pe/src/App.js
+++ holidaze-pe/src/App.js
@@ -5,11 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/home/home";
 import LogIn from "./pages/login/login";
-/* import Register from "./pages/register/register";
-import Venue from "./pages/specific/specific";
-import Venues from "./pages/venues/venues";
-import Profile from "./pages/profile/profile";
-import RouteNotFound from "./pages/notfound/notfound"; */
+import Register from "./pages/register/register";
 
 function App() {
   return (
@@ -17,11 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="login" element={<LogIn />} />
-{/*         <Route path="register" element={<Register />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="venues" element={<Venues />} />
-        <Route path="specific/:id" element={<Venue />} />
-        <Route path="*" element={<RouteNotFound />} /> */}
+        <Route path="register" element={<Register />} />
       </Route>
     </Routes>
   );
